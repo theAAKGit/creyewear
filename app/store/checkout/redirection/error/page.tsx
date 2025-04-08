f74ef4343c088b0669bdea82d9a050c411502519
theJRFGitHub <jrf2421@gmail.com>
2025-04-07 18:55:03 -0700

    clip

--- app/store/checkout/redirection/error/page.tsx
+++ app/store/checkout/redirection/error/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import Link from "next/link";
+
 export default function ErrorPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-100">
       <div className="text-center p-8 bg-white rounded shadow-md">
         <h1 className="text-3xl font-bold text-red-600">¡Error en el Pago!</h1>
         <p className="text-gray-700 mt-4">Hubo un problema al procesar tu pago. Intenta nuevamente.</p>
-        <a href="/store/checkout" className="mt-6 inline-block px-4 py-2 bg-red-500 text-white rounded">
-          Volver a la página de pago
-        </a>
+        <Link href="/store/">Volver a la pagina de pago</Link>
       </div>
     </div>
   );
--- app/store/checkout/redirection/success/page.tsx
+++ app/store/checkout/redirection/success/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import Link from "next/link";
+
 export default function SuccessPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-green-100">
       <div className="text-center p-8 bg-white rounded shadow-md">
         <h1 className="text-3xl font-bold text-green-600">¡Pago Exitoso!</h1>
         <p className="text-gray-700 mt-4">Tu pago ha sido procesado correctamente.</p>
-        <a href="/store" className="mt-6 inline-block px-4 py-2 bg-green-500 text-white rounded">
-          Volver a la tienda
-        </a>
+        <Link href="/store/">Volver a la tienda</Link>
       </div>
     </div>
   );
